d9d3d6e67b5a7edbba8d5339042516e6ecc6023e
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-07-15 15:38:43 +0530

    Added UserList Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
-import Card from "../UI/Card";
-import classes from "./AddUser.module.css";
-import Button from "../UI/Button";
 
-function AddUser(props) {
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
   const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value);
   };
@@ -14,25 +29,10 @@ function AddUser(props) {
     setEnteredAge(event.target.value);
   };
 
-  const addUserHandler = (event) => {
-    event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-      return console.log("Please enter Valid ");
-    }
-
-    if (+enteredAge < 1) {
-      return console.log("Please enter Age Greater Than 1");
-    }
-
-    setEnteredUsername("");
-    setEnteredAge("");
-    console.log(enteredUsername, enteredAge);
-  };
-
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Usename</label>
+        <label htmlFor="username">Username</label>
         <input
           id="username"
           type="text"
@@ -50,6 +50,6 @@ function AddUser(props) {
       </form>
     </Card>
   );
-}
+};
 
 export default AddUser;
