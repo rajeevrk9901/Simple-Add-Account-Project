450976b76c23beed9a53916d83fed658584c7d7e
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-07-15 13:42:21 +0530

    After Clicking Empty Input Field

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 function AddUser(props) {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    setEnteredUsername("");
+    setEnteredAge("");
+    console.log(enteredUsername, enteredAge);
   };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Usename</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
